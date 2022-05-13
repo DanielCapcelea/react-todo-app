e7fb0eced06d2e9a809e5036c8441ff87032789e
Daniel <danielcapcelea@mail.ru>
2022-05-13 18:12:26 +0200

    redux state & save in localStorage

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,31 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineClose } from 'react-icons/md';
-import styles from '../styles/modules/modal.module.scss';
+import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import { toast } from 'react-hot-toast';
 import Button from './Button';
+import { addTodo } from '../redux/todoSlice';
+import styles from '../styles/modules/modal.module.scss';
 
 function TodoModal({ modalOpen, setModalOpen }) {
+    const [title, setTitle] = useState('');
+    const [status, setStatus] = useState('incomplete');
+
+    const dispatch = useDispatch();
+
+    const onClose = () => {
+        setModalOpen(false);
+        setTitle('');
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (title && status) {
+            dispatch(
+                addTodo({
+                    id: uuid(),
+                    title,
+                    status,
+                    time: new Date().toLocaleDateString(),
+                })
+            );
+            toast.success('Task Added Successfully');
+            setModalOpen(false);
+        } else {
+            toast.error('Title should not be empty');
+        }
+    };
+
     return (
         modalOpen && (
             <div className={styles.wrapper}>
                 <div className={styles.container}>
                     <div
                         className={styles.closeButton}
-                        onClick={() => setModalOpen(false)}
+                        onClick={onClose}
                         onKeyDown={() => setModalOpen(false)}
                         tabIndex={0}
                         role="button"
                     >
                         <MdOutlineClose />
                     </div>
-                    <form className={styles.form}>
+                    <form
+                        className={styles.form}
+                        onSubmit={(event) => handleSubmit(event)}
+                    >
                         <h1 className={styles.formTitle}>Add Task</h1>
                         <label htmlFor="title">
                             Title
-                            <input type="text" id="title" />
+                            <input
+                                type="text"
+                                id="title"
+                                value={title}
+                                onChange={(event) =>
+                                    setTitle(event.target.value)
+                                }
+                            />
                         </label>
                         <label htmlFor="status">
                             Status
-                            <select name="status" id="status">
+                            <select
+                                name="status"
+                                id="status"
+                                value={status}
+                                onChange={(event) =>
+                                    setStatus(event.target.value)
+                                }
+                            >
                                 <option value="incomplete">Incomplete</option>
                                 <option value="complete">Complete</option>
                             </select>
@@ -36,7 +85,7 @@ function TodoModal({ modalOpen, setModalOpen }) {
                             </Button>
                             <Button
                                 variant="secondary"
-                                onClick={() => setModalOpen(false)}
+                                onClick={onClose}
                                 onKeyDown={() => setModalOpen(false)}
                             >
                                 Cancel
