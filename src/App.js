f2992501b02b12a7c120fb29379ab86d7c6e4560
Daniel <danielcapcelea@mail.ru>
2022-05-13 02:45:59 +0200

    button component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import style from './styles/modules/app.module.scss';
 import AppHeader from './components/AppHeader';
 
 function App() {
-  return (
-    <div className="container">
-      <PageTitle />
-      <div className={style.app__wrapper}>
-        <AppHeader />
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <PageTitle />
+            <div className={style.app__wrapper}>
+                <AppHeader />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Button from './Button';
 
 function AppHeader() {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Header</h1>
+            <Button variant="primary">Click</Button>
+        </div>
+    );
 }
 
 export default AppHeader;
