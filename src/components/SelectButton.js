898cc4d762d996fe860307e218e06db3099ab9fe
Daniel <danielcapcelea@mail.ru>
2022-05-17 12:55:26 +0200

    fixing styles

--- src/components/SelectButton.js
+++ src/components/SelectButton.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { RiArrowDownSFill } from 'react-icons/ri';
 import { getClasses } from '../utils/getClasses';
 import styles from '../styles/modules/button.module.scss';
 
 function SelectButton({ children, id, ...rest }) {
     return (
-        <select
-            id={id}
-            className={getClasses([styles.button, styles.button__select])}
-            {...rest}
-        >
-            {children}
-        </select>
+        <div className={styles.custom__select}>
+            <select
+                id={id}
+                className={getClasses([styles.button, styles.button__select])}
+                {...rest}
+            >
+                {children}
+            </select>
+            <span className={styles.arrow}>
+                <RiArrowDownSFill />
+            </span>
+        </div>
     );
 }
 
